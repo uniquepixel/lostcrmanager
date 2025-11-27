f5bd4a0b0e5929c852a1a3347c9b5741956135d8
uniquepixel <joni.dormann@gmail.com>
2025-11-27 23:40:49 +0100

    formalities

--- src/main/java/lostcrmanager/Bot.java
+++ src/main/java/lostcrmanager/Bot.java
@@ -555,7 +555,7 @@ public class Bot extends ListenerAdapter {
 			return;
 		}
 
-		channel.sendMessage(messages.get(index)).queue(msg -> {
+		channel.sendMessage(messages.get(index)).queue(_ -> {
 			// Send next message
 			sendMessagesSequentially(channel, messages, index + 1, reminderId, clantag);
 		}, error -> {
